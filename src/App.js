88c7931bd068491dfc118635e90ae8ffb30d2e89
Niloy173 <niloysaha173@gmail.com>
2022-12-05 04:10:56 +0600

    made some changes

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 
 // components
 import About from './about/About';
+import Contact from './contact/Contact';
 import Footer from './footer/Footer';
 import Header from './header/Header';
 import Nav from './nav/Nav';
 import Project from './project/Project';
-
+import Skill from './skill/Skill';
+// import Experience from './Work/Experience';
 
 const App = () => {
   return (
@@ -15,7 +17,9 @@ const App = () => {
      <Header/>
      <Nav/>
      <About/>
+     <Skill/>
      <Project/>
+     <Contact/>
      <Footer/>
     
     </>
