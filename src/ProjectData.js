5ea4401c1a4f00a4d44095028cd71e231876b5dc
Niloy173 <niloysaha173@gmail.com>
2022-12-08 17:06:12 +0600

    seperated project data from component (project)

---
+++ src/ProjectData.js
@@ -0,0 +1,73 @@
+
+// image files
+import project1 from './assets/autoFlash.jpg'
+import project2 from './assets/chatApp.jpg'
+import project7 from './assets/fifa.jpg'
+import project3 from './assets/fundRaiser.jpg'
+import project4 from './assets/mathBook.jpg'
+import project5 from './assets/mustardOil.jpg'
+import project6 from './assets/sortingVisualization.jpg'
+
+
+const imageArray = [
+  {
+    id: 1,
+    title: "AUTO FLASH",
+    image: project1,
+    description : "An useful application which is easily turned on the flashlight to the rear camera based on some important Events",
+    gitLink: "https://github.com/Niloy173/AutoFlash",
+    liveLink: "https://play.google.com/store/apps/details?id=com.teamaspirant.autoflash"
+  },
+  {
+    id: 2,
+    title: "CHAT APPLICATION",
+    image: project2,
+    description : "A real time chat environment so feel free to explore yourself by chating with your friends or any unknown user of which you want to conect",
+    gitLink: "https://github.com/Niloy173/Chat-Application",
+    liveLink: "https://chatapp-j8xc.onrender.com/"
+  },
+  {
+    id: 3,
+    title: "Easy Fund",
+    image: project3,
+    description : "A crowdfunding application or platform of hope, want, desire and anticipate where help each other's dream to be successful by sharing indiviual story, business or creation",
+    gitLink: "https://github.com/Niloy173/EasyFund",
+    liveLink: "https://easyfund.onrender.com/"
+  },
+  {
+    id:4,
+    title: "MATH GUIDE BOOK",
+    image: project4,
+    description : "MathGuideBook - This is a Simple Math Solution App for Class 9-10 in BD",
+    gitLink: "https://github.com/Niloy173/MathGuideBook",
+    liveLink: "https://play.google.com/store/apps/details?id=com.teamaspirant.mathguidebook"
+  },
+
+  {
+    id:5,
+    title: "MUSTARD OIL",
+    image: project5,
+    description : "Design template for single product e-commerce platform for buy & sell things",
+    gitLink: "https://github.com/Niloy173/Muatard-Oil",
+    liveLink: "https://niloy173.github.io/Muatard-Oil/"
+  },
+  {
+    id:6,
+    title: "SORTING VISUALIZATION",
+    image: project6,
+    description : "Sort the pixel to get the Original Picture from the Unsorted Pixels",
+    gitLink: "https://github.com/Niloy173/Sorting-Visualization",
+    liveLink: "https://www.youtube.com/watch?v=bA_P4yi-w-c&ab_channel=NiloySaha"
+  },
+
+  {
+    id:7,
+    title: "FIFA WORLD CUP - QUIZ",
+    image: project7,
+    description : "This is a quiz application project based on the football history or fifa events",
+    gitLink: "https://github.com/Niloy173/Fifa-World-Cup-Quiz---React",
+    liveLink: "https://fifa-world-cup-quiz.netlify.app/"
+  }
+];
+
+export default imageArray
